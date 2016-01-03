a9ab82d6373cf552c5f67b31781b93b0228d52e8
John Mayberry <jmaybe@mac.com>
2016-01-02 18:12:36 -0600

    Added comments

--- media.py
+++ media.py
@@ -1,11 +1,20 @@
+# imports module (or functionality) of webbrowser
 import webbrowser
 
+# creating a class that will be used in another program
 class Movie():
+    
+    # This is the constructor that provides the foundation for the instances and
+    # their variables to come
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+
+        # These are instance variables. They can be whatever we want them to be, but
+        # must match in number and order when creating the Instance.
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+    # This is the Instance Method
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
