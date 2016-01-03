390dec96da6bca48b97174f24c29955cc7c3680f
John Mayberry <jmaybe@mac.com>
2016-01-02 20:05:30 -0600

    PEP8 Online corrections

--- entertainment_center.py
+++ entertainment_center.py
@@ -8,27 +8,28 @@ import media
 # Adding each instance and the instance variable that go with each
 
 holy_grail = media.Movie("Monty Python and the Holy Grail",
-                         "You are a very silly person if you don't agree this is the greatest movie ever made.",
-                         "http://www.stonelibertystation.com/wp-content/uploads/2013/07/monty-python-and-the-holy-grail-poster-artwork-terry-gilliam-eric-idle-graham-chapman.jpg",
+                         "You are a very silly person if you don't agree"
+                         " this is the greatest movie ever made.",
+                         "http://www.stonelibertystation.com/wp-content/uploads/2013/07/monty-python-and-the-holy-grail-poster-artwork-terry-gilliam-eric-idle-graham-chapman.jpg",  # NOQA
                          "https://www.youtube.com/watch?v=urRkGvhXc8w",
-                             # added another instance variable for additional comments
-                         "1975 - Directed by Gilliam and Jones. Written by and starring: Chapman, Cleese, Idle, Gilliam, Jones, Palin")
+# added another instance variable for additional comments
+                         "1975 - Directed by Gilliam and Jones. Written by and starring: Chapman, Cleese, Idle, Gilliam, Jones, Palin")  # NOQA
 
 watchmen = media.Movie("The Watchmen",
                         "Steam punk superheroes...",
-                        "http://www.watchmencomicmovie.com/posters/watchmen-theatrical-poster-big.jpg",
+                        "http://www.watchmencomicmovie.com/posters/watchmen-theatrical-poster-big.jpg",  # NOQA
                         "https://www.youtube.com/watch?v=ONQ3Zgy195Y",
                        "2009 - Directed by Jack Snyder.")
 
 star_wars = media.Movie("Star Wars",
                         "Cowboys in space.",
-                        "http://cdn.akihabaranews.com/sites/default/files/star_wars_a_new_hope_japanese_poster.2.jpg",
+                        "http://cdn.akihabaranews.com/sites/default/files/star_wars_a_new_hope_japanese_poster.2.jpg",  # NOQA
                         "https://www.youtube.com/watch?v=1g3_CFmnU7k",
                         "1977 - Directed by He Who Must Not Be Named.")
 
 pulp_fiction = media.Movie("Pulp Fiction",
                            "One of my all-time favorites",
-                           "https://s-media-cache-ak0.pinimg.com/236x/8a/50/82/8a5082e4da061a777a32c9ffaa6d542a.jpg",
+                           "https://s-media-cache-ak0.pinimg.com/236x/8a/50/82/8a5082e4da061a777a32c9ffaa6d542a.jpg",  # NOQA
                            "https://www.youtube.com/watch?v=ewlwcEBTvcg",
                            "1994 - Directed by Quentin Tarantino.")
 
@@ -36,5 +37,5 @@ pulp_fiction = media.Movie("Pulp Fiction",
 # creating a list of movies to be included when page is opened
 movies = [holy_grail, watchmen, pulp_fiction, star_wars]
 
-# initiates the web page creation including those movies added to the 'movie' list
+# initiates the web page
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,15 +1,16 @@
 # imports module (or functionality) of webbrowser
 import webbrowser
 
+
 # creating a class that will be used in another program
 class Movie():
-    
-    # This is the constructor that provides the foundation for the instances and
-    # their variables to come
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, movie_comments):
 
-        # These are instance variables. They can be whatever we want them to be, but
-        # must match in number and order when creating the Instance.
+    # constructor
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube, movie_comments):
+
+        # These are instance variables. They can be whatever we want them to
+        # be, but must match in number and order when creating the Instance.
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
